ab753e1eca7e5e7584235de6dd0a5a703094756b
Juan  França <juanfranca123@gmail.com>
2023-08-31 20:02:11 -0300

    FBV to CBV and TemplateChanges

--- cars/urls.py
+++ cars/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path 
-from cars.views import car_home, create_car
+from cars.views import Car_home, Create_car, Detail_car, Update_car, Delete_car
 
 app_name ='cars'
 
 urlpatterns = [
-    path('', car_home, name='home'),
-    path('create_car/', create_car, name='create_car'),       
+    path('', Car_home.as_view(), name='home'),
+    path('create_car/', Create_car.as_view(), name='create_car'),
+    path('detail_car/<int:pk>', Detail_car.as_view(), name='detail_car'),
+    path('update/<int:pk>', Update_car.as_view(), name='update_car'),
+    path('delete/<int:pk>', Delete_car.as_view(), name='delete_car'),             
 ]
 
 
--- cars/views.py
+++ cars/views.py
@@ -1,28 +1,42 @@
 from django.shortcuts import  render, redirect
 from cars.models import Car
 from cars.forms import CarForm
-from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-def car_home(request):
-   url_search = request.GET.get('search')
-   cars = Car.objects.all().order_by('-year')
-   form = CarForm()
-   if url_search:
-      cars = Car.objects.filter(model__icontains=url_search)
-   return render( request, 
-                  'home.html', 
-                  {'cars':cars, 'form':form})
 
-@login_required
-def create_car(request):
-    if request.method == "POST":
-        form = CarForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('cars:home')
-    else:
-        form = CarForm()
-        
-    return render(request, "create_car.html", {"form": form})
+class Car_home(ListView):
+    template_name ='home.html'
+    model = Car
+    context_object_name = 'cars'
+
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get('search')
+        if search:
+            cars = cars.filter(model__icontains=search)
+        return cars
+
+class Create_car(LoginRequiredMixin, CreateView):
+    form_class = CarForm
+    template_name = 'create_car.html'
+    success_url = '/'
     
 
+class Detail_car(DetailView):
+    template_name = 'detail_car.html'
+    model = Car
+    context_object_name = 'cars'
+    
+
+class Update_car(UpdateView):
+    template_name = 'edit_car.html'
+    model = Car
+    fields = '__all__'
+    context_object_name = 'cars'
+    success_url = '/'
+
+class Delete_car(DeleteView):
+    model = Car
+    template_name = 'confirm_delete.html'
+    success_url = '/'
